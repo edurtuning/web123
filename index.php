d6ea83c5a917294119dd2caa3041ba23635ed59c
EDU <>
2014-04-19 04:14:13 +0200

    probando desde netbeans

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Titulo modificado</title>
+    </head>
+    <body>
+        <?php
+        // esto ha sido modificado...
+        ?>
+    </body>
+</html>
