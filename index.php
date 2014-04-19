fa7899104762ba4a4e50dcc0612a61057118c1c2
dudu <dudu@du>
2014-04-19 20:44:51 +0200

    Actualizando los errores...

--- index.php
+++ index.php
@@ -7,13 +7,40 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Web 123</title>
+        <title>Calculadora de tamaño de pantalla</title>
     </head>
     <body>
         <?php
-            echo "hola web 123...";
-            echo "<br>";
-            echo "no me creo que esto funcione!!!!";
+            if(isset($_POST['pulgadas'])){
+                
+                $pulgadas=$_POST['pulgadas'];
+                $formato=$_POST['formato'];
+                
+                //Calculamos el angulo en radianes
+                $angulo=  atan($formato);
+                
+                $PULGADAS_EN_CM = 2.54;
+                $diagonalEnCm = $PULGADAS_EN_CM * $pulgadas;
+                $alto = sin($angulo) * $diagonalEnCm;
+                $ancho = cos($angulo) * $diagonalEnCm;
+                
+                echo "<div>Size: ".$alto." x ".$ancho."</div>";
+                
+            }
         ?>
+        <form action="" method="post">
+            
+            <label for="pulgadas">Diagonal size(inch):</label>
+            <input name="pulgadas" id="pulgadas" type="number" min="0" step="0.1" max="9999999">
+            <br>
+            <label for="formato">Formato de pantalla:</label>
+            <select name="formato" id="formato">
+                <option value="1.77777777">16:9 HD Phones</option>
+                <option value="1.5">3:2 (iPhone, 480x320)</option>
+                <option value="1.33333333">4:3 (iPad, Galaxy Y, 320*240)</option>
+            </select>
+            <br>
+            <input type="submit" id="enviar" value="Calcule Size">
+        </form>   
     </body>
 </html>
